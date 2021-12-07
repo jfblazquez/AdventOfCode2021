3a423664be05f9bb90d042af5358f842901c505a
jfblazquez <jfblazquez@users.noreply.github.com>
2021-12-07 12:24:27 +0100

    day7

--- AdventOfCode2021/Day7.cpp
+++ AdventOfCode2021/Day7.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <fstream>
-#include "NotImplementedException.h"
+#include <string>
+#include <vector>
+#include <limits>
+#include <numeric>
 using namespace std;
 
 #include "Day7.h"
@@ -11,9 +14,41 @@ Day7::Day7() {
 }
 
 int Day7::puzzle1() {
-    throw NotImplementedException();
+    return solve(true);
 }
 
 int Day7::puzzle2() {
-    throw NotImplementedException();
+    return solve(false);
+}
+
+int Day7::solve(bool basicFuel) {
+    ifstream ifs(filename, ios::binary);
+    vector<int> crabs;
+    string strstream;
+
+    while (getline(ifs, strstream, ',')) {
+        crabs.push_back(stoi(strstream));
+    }
+
+    //hint start average
+    int hMove = std::accumulate(crabs.begin(), crabs.end(), 0) / crabs.size(); 
+
+    while (true) {
+        int fuel{}, fuelup{}, fueldown{};
+        for (auto crab : crabs) {     
+            fuelup += fuelForPos(abs(crab - (hMove + 1)), basicFuel);            
+            fuel += fuelForPos(abs(crab - hMove), basicFuel);
+            fueldown += fuelForPos(abs(crab - (hMove - 1)), basicFuel);
+        }
+        if (fuel < fuelup && fuel < fueldown) return fuel;
+        if (fuelup < fuel) hMove++;
+        else hMove--;
+    }
+
+    return 0;
+};
+
+int Day7::fuelForPos(int pos, bool basicFuel) {
+    if (basicFuel) return pos;
+    return (pos * (pos + 1)) / 2;
 }
