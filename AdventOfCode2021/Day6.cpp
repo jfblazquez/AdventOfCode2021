db8f313f924e8a4d72e4d35f298fac334e0fe9c3
jfblazquez <jfblazquez@users.noreply.github.com>
2021-12-09 18:33:22 +0100

    static analysis code

--- AdventOfCode2021/Day6.cpp
+++ AdventOfCode2021/Day6.cpp
@@ -14,12 +14,12 @@ Day6::Day6() {
 }
 
 int Day6::puzzle1() {
-    return solve(80);
+    return (int)solve(80);
 }
 
 int Day6::puzzle2() {
     long long ret = solve(256);
-    cout << "Day: 6 puzzle2: " << ret << endl;
+    cout << "Day 6 puzzle 2: " << ret << endl;
     return -1; //warning: bigger than int (the valid answer is the output of this method)
 }
 
@@ -39,7 +39,7 @@ long long Day6::solve(int days)
         std::array<long long, 9> lfageDay = { 0,0,0,0,0,0,0,0,0 };
         lfageDay[6] += lfage[0];
         lfageDay[8] += lfage[0];
-        for (int age = 1; age <= 8; age++) {
+        for (size_t age = 1; age <= 8; age++) {
             lfageDay[age - 1] += lfage[age];
         }
         lfage = lfageDay;
