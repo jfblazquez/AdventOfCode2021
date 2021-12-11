777e5ba7eb070f74bd40d2d2e7dcee728378c6f1
jfblazquez <jfblazquez@users.noreply.github.com>
2021-12-11 22:36:26 +0100

    day11

--- AdventOfCode2021/Day11.h
+++ AdventOfCode2021/Day11.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <array>
 #include "BaseDay.h"
 
 class Day11 : 
@@ -7,5 +8,18 @@ public:
     Day11();
     int puzzle1();
     int puzzle2();
+private:
+    void fillData();
+    int doStep(int);
+    void energyByOne();
+    void increaseAdjacent(int r, int c);
+    void increaseCurrent(int r, int c, bool spread);
+    void doFlash();
+    int countFlash();
+    int resetEnergy();
+    int dropEnergyOnFlash();
+    bool dropEnergyCurrent(int, int);
+    array<array <int, 10>, 10> sea;
+    array<array <int, 10>, 10> flash;
 };
 
