5a8e235e133bf8a6fc63ce1bbeb2c6ea34779589
jfblazquez <jfblazquez@users.noreply.github.com>
2021-12-17 20:07:35 +0100

    day 17 part1

--- AdventOfCode2021/Day17.cpp
+++ AdventOfCode2021/Day17.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
 #include "NotImplementedException.h"
 using namespace std;
 
@@ -8,12 +9,106 @@ using namespace std;
 Day17::Day17() {
     day = 17;
     filename = "input/Day17.txt";
+    x1 = 185;
+    x2 = 221;
+    y1 = -74;
+    y2 = -122;
+    
+    /*x1 = 20;
+    x2 = 30;
+    y1 = -5;
+    y2 = -10;*/
+
 }
 
 int Day17::puzzle1() {
-    throw NotImplementedException();
+    int minXVel = (int)ceil(sqrt((2 * x1)+0.25) -0.5);
+    int maxXVel = minXVel; //x2 /2
+    while (hitxTarget(maxXVel + 1)) {
+        maxXVel++;
+    }
+
+    bool minYFound = false;
+    int minYVel = 0; 
+    int maxYVel = 0;
+    for (int yRange = 0; yRange < 1000; yRange++) {
+
+        if (!minYFound && hityTarget(yRange)) {
+            minYFound = true;
+            minYVel = 0;
+        }
+        else if (minYFound && hityTarget(yRange)) {
+            maxYVel = yRange;
+            
+        }
+    }
+
+    int y = 0;
+    bool done = false;
+    for (int sx = minXVel;!done && sx <= maxXVel; sx++) {
+        for (int sy = maxYVel;!done && sy >= minYVel; sy--) {
+            if (makeHit(sx, sy)) {
+                y = sy;
+                done = true;
+            }
+        }
+    }    
+
+    return (y*(y+1))/2;
 }
 
 int Day17::puzzle2() {
     throw NotImplementedException();
 }
+
+bool Day17::hitxTarget(int initialSpeed)
+{
+    int speed = initialSpeed;
+    int val = 0;
+    while (val < x1 && speed >= 0) {
+        val += speed;
+        speed--;        
+    }
+    return val >= x1 && val <= x2;
+}
+
+bool Day17::hityTarget(int initialSpeed)
+{
+    int speed = initialSpeed;
+    int val = 0;
+    while (val > y1) {
+        val += speed;
+        speed--;
+    }
+    return val <= y1 && val >= y2;
+}
+
+bool Day17::isHit(int x, int y)
+{
+    return x >= x1 && x <= x2 &&
+        y <= y1 && y >= y2;
+}
+
+bool Day17::makeHit(int initialsx, int initialsy)
+{
+    int sx = initialsx;
+    int sy = initialsy;
+    int x = 0, y = 0;
+    while (inRange(x, y)) {
+        if (isHit(x, y)) return true;
+        x += sx;
+        y += sy;
+        sx = sx > 0 ? sx - 1 : 0;
+        sy--;
+    }
+    return false;
+}
+
+bool Day17::inRange(int x, int y)
+{
+    return x <= x2 && y >= y2;
+}
+
+
+
+
--- AdventOfCode2021/Day17.h
+++ AdventOfCode2021/Day17.h
@@ -7,5 +7,14 @@ public:
     Day17();
     int puzzle1();
     int puzzle2();
+private:
+
+    bool hitxTarget(int initial);
+    bool hityTarget(int initial);
+    bool isHit(int x, int y);
+    bool makeHit(int sx, int sy);
+    bool inRange(int x, int y);
+    int x1{}, x2{}, y1{}, y2{};
+
 };
 
