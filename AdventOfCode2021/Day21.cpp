40f45c4bf397b4505931b3113fa026986666e790
jfblazquez <jfblazquez@users.noreply.github.com>
2021-12-29 22:38:48 +0100

    #day21 part 1

--- AdventOfCode2021/Day21.cpp
+++ AdventOfCode2021/Day21.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "NotImplementedException.h"
 using namespace std;
 
@@ -11,9 +12,44 @@ Day21::Day21() {
 }
 
 int Day21::puzzle1() {
-    throw NotImplementedException();
+    fillData();
+    bool finish = false;
+    while (!finish) {
+        int rolls = getDiceVal() + getDiceVal() + getDiceVal();        
+        scoreP1 += moveNewPos(posP1, rolls);
+        finish = scoreP1 >= 1000;
+        if (!finish) {
+            rolls = getDiceVal() + getDiceVal() + getDiceVal();
+            scoreP2 += moveNewPos(posP2, rolls);
+            finish = scoreP2 >= 1000;
+        }
+    }
+
+    int ret = min(scoreP1, scoreP2) * numRolls;
+    return ret;
 }
+
 
 int Day21::puzzle2() {
     throw NotImplementedException();
+}
+
+int Day21::getDiceVal() {
+    numRolls++;
+    diceVal = (diceVal % 100) + 1;
+    return diceVal;
+}
+
+int Day21::moveNewPos(int& playerPos, int value) {
+    playerPos = ((playerPos + value - 1) % 10) + 1;
+    return playerPos;
+}
+
+void Day21::fillData() {
+    ifstream ifs(filename);
+    string line;
+    getline(ifs, line);
+    posP1 = *(line.end() - 1) - '0';
+    getline(ifs, line);
+    posP2 = *(line.end() - 1) - '0';
 }
--- AdventOfCode2021/Day21.h
+++ AdventOfCode2021/Day21.h
@@ -6,6 +6,20 @@ class Day21 :
 public:
     Day21();
     int puzzle1();
+    void fillData();
     int puzzle2();
+
+private:
+    int getDiceVal();
+    int moveNewPos(int& playerPos, int value);
+
+    int diceVal{100};
+    int scoreP1{};
+    int scoreP2{};
+    int posP1{};
+    int posP2{};
+    int numRolls{};
+    
 };
+
 
